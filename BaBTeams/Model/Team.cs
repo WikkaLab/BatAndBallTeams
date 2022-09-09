22562c670f9d2eda0145ede2d6b042dc760194ed
Elena G Blanco <elena.guzbla@gmail.com>
2022-09-09 11:47:57 +0200

    🔍 Add details page

--- BaBTeams/Model/Team.cs
+++ BaBTeams/Model/Team.cs
@@ -5,5 +5,7 @@
         public string Name { get; set; }
         public string Location { get; set; }
         public string Image { get; set; }
+        public int FoundationYear { get; set; }
+        public string Category { get; set; }
     }
 }
